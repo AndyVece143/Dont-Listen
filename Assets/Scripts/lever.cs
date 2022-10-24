23e70c014f7c54ef2a6346d7f5eba60d4e49da04
AndyVece143 <89553507+AndyVece143@users.noreply.github.com>
2022-10-24 13:09:52 -0400

    Lever Model, Texture, and Animation

--- Assets/Scripts/lever.cs
+++ Assets/Scripts/lever.cs
@@ -1,11 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class lever : MonoBehaviour
 {
     public bool currentState = false;
-    
+    private Animation anim;
+    //private Animator anim;
+
+    public void Start()
+    {
+        //anim = GetComponent<Animator>();
+        anim = GetComponent<Animation>();
+    }
+
+
     public void pulled()
     {
         if (!currentState)
@@ -13,12 +24,15 @@ public class lever : MonoBehaviour
             currentState = true;
 
             // animate lever turning on
+            anim.Play("Pull1");
+
         }
         else 
         {
             currentState = false;
 
-            // animate lever turning off
+            // animate lever turning of
+            anim.Play("Pull2");
         }
     }
 }
