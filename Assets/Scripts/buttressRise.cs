1822d1a3368b30d664c3a9ceb8a4a1295a6fee1a
Boy-Chase <70419012+Boy-Chase@users.noreply.github.com>
2022-10-25 14:34:07 -0400

    added candleabra integration to final puzzle

--- Assets/Scripts/buttressRise.cs
+++ Assets/Scripts/buttressRise.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class buttressRise : MonoBehaviour
 {
-    public GameObject pedestal;
+    public GameObject pedestal1;
+    public GameObject pedestal2;
 
     // Update is called once per frame
     void Update()
     {
-        if (pedestal.GetComponent<itemDestination>().activator && this.gameObject.transform.position.y < 20.0f)
+        if (pedestal1.GetComponent<itemDestination>().activator && pedestal2.GetComponent<itemDestination>().activator && this.gameObject.transform.position.y < 20.0f)
         {
             this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y + 0.02f, this.gameObject.transform.position.z);
         }
